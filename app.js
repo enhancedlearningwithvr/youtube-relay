323abebf3f302218d4b680ab43583392bfcbc5d3
tharunoptimus <tharunoptimus@outlook.com>
2023-04-08 17:29:50 +0530

    Added REST endpoint `/:id` to relay YouTube videos

--- app.js
+++ app.js
@@ -16,6 +16,28 @@ app.get("/", cors, (_, res) => {
 	res.send("API Online!")
 })
 
+app.get("/:id", cors, async (req, res) => {
+	try {
+		const videoId = req.params.id
+
+		const apiUrl = `https://shay.loan/${videoId}`
+		const response = await fetch(apiUrl)
+
+		if (!response.ok) {
+			throw new Error("Error downloading the video file")
+		}
+
+		res.setHeader("Content-Type", "video/mp4")
+		res.setHeader(
+			"Content-Disposition",
+			`attachment; filename="${videoId}.mp4"`
+		)
+		response.body.pipe(res)
+	} catch (error) {
+		console.error(error)
+		res.status(500).send(error.message)
+	}
+})
 
 app.get("*", cors, (_, res) => {
     res.status(404).send("Not Found")
